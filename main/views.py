4061af8bf580269d34458cdafdba0dd9f5299c43
Mr.LaG <it.is.mr.lag@gmail.com>
2021-08-08 16:41:59 +0500

    Adding handlers inside html files

--- main/views.py
+++ main/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    info = {
+        'title': "index.html(переданные изнаутри параметры)",
+        'values': ['Yo', 'Hello', 'Mr.LaG'],
+        'objects': {
+            'number': 99,
+            'info': 'как вытащить значение из многомерного массива',
+        }
+    }
+    return render(request, 'main/index.html', info)
 
 
 def about(request):
-    return render(request, 'main/about.html')
+    info = {
+        'title': "index.html(переданные изнаутри параметры)",
+        'values': ['It', 'Is', 'Mr.LaG'],
+        'objects': {
+            'number': "eighty",
+            'info': 'как вытащить значение из многомерного массива версия2',
+        }
+    }
+    return render(request, 'main/about.html', info)
