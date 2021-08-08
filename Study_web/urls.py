89af33b254918abd5823e9814094e0cad1e82955
Mr.LaG <it.is.mr.lag@gmail.com>
2021-08-08 15:15:17 +0500

    Web-Style adding

--- Study_web/urls.py
+++ Study_web/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 #todo как я понял, здесь указывается путь и привязанное к нему "приложение" (https://.../admin/)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
